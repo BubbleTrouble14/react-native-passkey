9487849053f44b5cdc5adb97c1b2202bbf1e1ddd
Ronald <ronald.goedeke@outlook.com>
2023-11-08 20:21:02 +0100

    changed the example a little

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,55 +1,80 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Button, TextInput, Alert } from 'react-native';
+import { Alert, Button, StyleSheet, View } from 'react-native';
 import { Passkey } from 'react-native-passkey';
 
-import RegRequest from './RegRequest.json';
-import AuthRequest from './AuthRequest.json';
-
 export default function App() {
-  const [email, setEmail] = React.useState('');
-
-  async function createAccount() {
+  async function register() {
     try {
-      const requestJson = {
-        // ...Retrieve request from server
-        ...RegRequest,
-      };
-
-      console.log(requestJson);
-
-      const result = await Passkey.register(requestJson);
-
-      console.log('Registration result: ', result);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  async function authenticateAccount() {
-    try {
-      const requestJson = {
-        // ...Retrieve request from server
-        ...AuthRequest,
-      };
-
-      const result = await Passkey.authenticate(requestJson);
-
-      console.log('Authentication result: ', result);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  async function createAccountWithSecurityKey() {
-    try {
-      const requestJson = {
-        // ...Retrieve request from server
-        ...RegRequest,
-      };
-
-      const result = await Passkey.register(requestJson, {
-        withSecurityKey: true,
+      // const result = await Passkey.register({
+      //   challenge: 'nhkQXfE59Jb97VyyNJkvDiXucMEvltduvcrDmGrODHY',
+      //   rp: {
+      //     name: 'CredMan App Test',
+      //     // id: 'test',
+      //     // id: 'kayak.de',
+      //     id: 'mintgarden.io',
+      //     // id: 'https://mintgarden.io/.well-known/assetlinks.json',
+      //   },
+      //   user: {
+      //     id: '2HzoHm_hY0CjuEESY9tY6-3SdjmNHOoNqaPDcZGzsr0',
+      //     name: 'helloandroid@gmail.com',
+      //     displayName: 'helloandroid@gmail.com',
+      //   },
+      //   pubKeyCredParams: [
+      //     {
+      //       type: 'public-key',
+      //       alg: -7,
+      //     },
+      //     {
+      //       type: 'public-key',
+      //       alg: -257,
+      //     },
+      //   ],
+      //   timeout: 1800000,
+      //   attestation: 'none',
+      //   excludeCredentials: [],
+      //   authenticatorSelection: {
+      //     authenticatorAttachment: 'platform',
+      //     requireResidentKey: true,
+      //     residentKey: 'required',
+      //     userVerification: 'required',
+      //   },
+      // });
+      const result = await Passkey.register({
+        challenge: 'test',
+        rp: {
+          name: 'Passkey Test',
+          id: 'mintgarden.io',
+        },
+        user: {
+          id: '2HzoHm_hY0CjuEESY9tY6-3SdjmNHOoNqaPDcZGzsr0',
+          name: 'Passkey Test',
+          displayName: 'Passkey Test',
+        },
+        pubKeyCredParams: [
+          {
+            type: 'public-key',
+            alg: -7,
+          },
+          {
+            type: 'public-key',
+            alg: -257,
+          },
+        ],
+        timeout: 1800000,
+        attestation: 'none',
+        excludeCredentials: [],
+        authenticatorSelection: {
+          authenticatorAttachment: 'platform',
+          requireResidentKey: true,
+          residentKey: 'required',
+          userVerification: 'required',
+        },
+        extensions: {
+          largeBlob: {
+            support: 'preferred',
+          },
+        },
       });
 
       console.log('Registration result: ', result);
@@ -58,15 +83,46 @@ export default function App() {
     }
   }
 
-  async function authenticateAccountWithSecurityKey() {
+  async function write() {
     try {
-      const requestJson = {
-        // ...Retrieve request from server
-        ...AuthRequest,
-      };
+      const result = await Passkey.authenticate({
+        // challenge: 'T1xCsnxM2DNL2KdK5CLa6fMhD7OBqho6syzInk_n-Uo',
+        // allowCredentials: [],
+        // timeout: 1800000,
+        // userVerification: 'required',
+        // rpId: 'mintgarden.io',
+        challenge: 'test',
+        allowCredentials: [],
+        timeout: 1800000,
+        userVerification: 'required',
+        rpId: 'mintgarden.io',
+        extensions: {
+          largeBlob: {
+            write:
+              'c3dhbGxvdyBjb2luIHdvcmsgbWVudSBjb2luIHNsZW5kZXIgb25saW5lIGNyYXRlciByaXR1YWwgc2ltcGxlIGZlbmNlIGVtcGxveSBwcm9tb3RlIHNpbGx5IGRhd24gaW52ZXN0IG1ham9yIGJ1bmtlciB3aWZlIGJlY29tZSBzdWJtaXQgY291cnNlIHZpcnR1YWwgdHJpYmU=',
+          },
+        },
+      });
 
-      const result = await Passkey.authenticate(requestJson, {
-        withSecurityKey: true,
+      console.log('Authentication result: ', result);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async function read() {
+    try {
+      const result = await Passkey.authenticate({
+        challenge: 'test',
+        allowCredentials: [],
+        timeout: 1800000,
+        userVerification: 'required',
+        rpId: 'mintgarden.io',
+        extensions: {
+          largeBlob: {
+            read: true,
+          },
+        },
       });
 
       console.log('Authentication result: ', result);
@@ -82,17 +138,9 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <TextInput placeholder="email" value={email} onChangeText={setEmail} />
-      <Button title="Create Account" onPress={createAccount} />
-      <Button
-        title="Create Account with Security Key"
-        onPress={createAccountWithSecurityKey}
-      />
-      <Button title="Authenticate" onPress={authenticateAccount} />
-      <Button
-        title="Authenticate with Security Key"
-        onPress={authenticateAccountWithSecurityKey}
-      />
+      <Button title="Register" onPress={register} />
+      <Button title="Write" onPress={write} />
+      <Button title="Read" onPress={read} />
       <Button title="isSupported?" onPress={isSupported} />
     </View>
   );
